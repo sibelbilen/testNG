94f27e0cbad70b39717972377258c40d4c7eb8ce
sibelbilen <sibelinko33@gmail.com>
2023-04-25 20:02:10 +0300

    .

--- src/test/java/Gunluklerim/C01_TestingKonu_notu.java
+++ src/test/java/Gunluklerim/C01_TestingKonu_notu.java
@@ -1,7 +1,45 @@
 package Gunluklerim;
 
+import org.testng.annotations.Test;
+
 public class C01_TestingKonu_notu {
 
+/*
+1-@Test:test case olusturmakicin kullanilir.
+2-
+ */
+@Test
+public void test01() {
+    System.out.println("Test 1");
+}
 
+    @Test
+    public void test02() {
+        System.out.println("Test 2");
+    }
 
+    @Test(groups = "regression test")
+    public void test03() {
+        System.out.println("Test 3");
+    }
+
+    @Test
+    public void test04() {
+        System.out.println("Test 4");
+    }
+
+    @Test(groups = "regression test")
+    public void test05() {
+        System.out.println("Test 5");
+    }
+
+    @Test
+    public void test06() {
+        System.out.println("Test 6");
+    }
+
+    @Test
+    public void test07() {
+        System.out.println("Test 7");
+    }
 }
