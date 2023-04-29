53584cc1766b2410830354678aeed75f27c15796
sibelbilen <sibelinko33@gmail.com>
2023-04-29 18:05:31 +0300

    .

--- src/test/java/techproed/tests/day24_Properties_Pages/C02_PageKullanımı.java
+++ src/test/java/techproed/tests/day24_Properties_Pages/C02_PageKullanımı.java
@@ -1,4 +1,23 @@
 package techproed.tests.day24_Properties_Pages;
 
+import org.testng.annotations.Test;
+
 public class C02_PageKullanımı {
+
+    @Test
+    public void test01() {
+
+        //https://opensource-demo.orangehrmlive.com/web/index.php/auth/login adrese gidelim
+
+
+
+
+        //kullaniciAdi, kullaniciSifre, submitButton elementlerini bul
+
+
+
+
+
+        //Login Testini basarili oldugunu test et
+    }
 }
