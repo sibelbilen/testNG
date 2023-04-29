89825bb20f0f46061217e5a67c4624489d383fdd
sibelbilen <sibelinko33@gmail.com>
2023-04-29 18:40:06 +0300

    .

--- src/test/java/techproed/pages/OpenSourcePage.java
+++ src/test/java/techproed/pages/OpenSourcePage.java
@@ -1,4 +1,21 @@
 package techproed.pages;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import techproed.utilies.Driver;
+
 public class OpenSourcePage {
+ /*
+Locate'lerimizi tek bir yerde duzenli bir sekilde tutabilmek icin;
+
+1-   ilk olarak olusturmus oldugumuz page class'imizda bir constructor olustururuz.
+2-PageFactory class'indan initelements methodu ile driver'i tanitiriz.
+3-@findBy notasyonu kullanarak locatelerimizi aliriz.
+    */
+
+    @Test
+    public void test01() {
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
 }
