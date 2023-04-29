e4117d9e132183b0286922d9ad864ba0dbe8f4d5
sibelbilen <sibelinko33@gmail.com>
2023-04-29 18:10:33 +0300

    .

--- src/test/java/techproed/tests/day24_Properties_Pages/C02_PageKullanımı.java
+++ src/test/java/techproed/tests/day24_Properties_Pages/C02_PageKullanımı.java
@@ -1,6 +1,8 @@
 package techproed.tests.day24_Properties_Pages;
 
 import org.testng.annotations.Test;
+import techproed.utilies.ConfigReader;
+import techproed.utilies.Driver;
 
 public class C02_PageKullanımı {
 
@@ -10,8 +12,10 @@ public class C02_PageKullanımı {
         //https://opensource-demo.orangehrmlive.com/web/index.php/auth/login adrese gidelim
 
 
-
-
+        Driver.getDriver().get(ConfigReader.getProperty("openSource_Url"));
+//        Yukardaki adres icin 20 adet test methodu olusturursak sonrasinda yukardaki url'de bir
+//        degisiklik oldugunda butun test methodlarindan tek tek url duzeltmek yerine bir kere .properties
+//        dosyamdan url'i duzeltirim ve bu bizim icin daha hizli daha duzenli ve daha kolay olur.
         //kullaniciAdi, kullaniciSifre, submitButton elementlerini bul
 
 
