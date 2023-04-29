1e8d732fd3d6a2a6cfdae0ac26fe9b805873b0d3
sibelbilen <sibelinko33@gmail.com>
2023-04-29 20:53:29 +0300

    .

--- src/test/java/techproed/tests/SmokeTest/negativeTest.java
+++ src/test/java/techproed/tests/SmokeTest/negativeTest.java
@@ -1,4 +1,33 @@
 package techproed.tests.SmokeTest;
 
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import techproed.pages.BlueRentalPage;
+import techproed.utilies.ConfigReader;
+import techproed.utilies.Driver;
+
 public class negativeTest {
+    /*
+        Description:
+        Kullanimda olmayan kullanıcı adi ve şifre ile giriş yapilamamali
+        Acceptance Criteria
+        Customer email: fake@bluerentalcars.com
+        Customer password: fakepass
+        Error:
+        User with email fake@bluerentalcars.com not found
+         */
+
+    @Test
+    public void testName() {
+        Driver.getDriver().get(ConfigReader.getProperty("blueRentACarUrl"));
+        BlueRentalPage blueRentalPage=new BlueRentalPage();
+        blueRentalPage.login.click();
+
+
+        blueRentalPage.email.sendKeys(ConfigReader.getProperty("fakeEmail"),
+                Keys.TAB,ConfigReader.getProperty("fakepass"),Keys.ENTER);
+
+
+
+    }
 }
