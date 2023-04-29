d732100f2921404632f0c344ffe3f61c84e53f31
sibelbilen <sibelinko33@gmail.com>
2023-04-29 20:25:29 +0300

    .

--- src/test/java/techproed/tests/SmokeTest/PositiveTest.java
+++ src/test/java/techproed/tests/SmokeTest/PositiveTest.java
@@ -1,4 +1,17 @@
 package techproed.tests.SmokeTest;
 
+import org.testng.annotations.Test;
+
 public class PositiveTest {
+
+    @Test
+    public void testName() {
+        /*
+        Acceptance Criteria:
+        Admin olarak, uygulamaya giriş yapabilmeliyim
+        https://www.bluerentalcars.com/
+        Admin email: jack@gmail.com
+        Admin password: 12345
+         */
+    }
 }
