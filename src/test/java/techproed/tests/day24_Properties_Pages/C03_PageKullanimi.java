017e39052c0e9015ffca4b7a9f98fd8a2575b9c6
sibelbilen <sibelinko33@gmail.com>
2023-04-29 19:58:29 +0300

    .

--- src/test/java/techproed/tests/day24_Properties_Pages/C03_PageKullanimi.java
+++ src/test/java/techproed/tests/day24_Properties_Pages/C03_PageKullanimi.java
@@ -11,21 +11,16 @@ public class C03_PageKullanimi {
 
     @Test
     public void test01() {
-        ////https://testcenter.techproeducation.com/index.php?page=form-authentication
+        //https://testcenter.techproeducation.com/index.php?page=form-authentication
         Driver.getDriver().get(ConfigReader.getProperty("testCenterUrl"));
-
-
-
         //Page object Model kullanarak sayfaya giriş yapildigini test edin
-        TestCenterTechproPage testCenterTechproPage=new TestCenterTechproPage();
-testCenterTechproPage.userName.sendKeys(ConfigReader.getProperty("kullaniciAdi"),Keys.TAB,ConfigReader.getProperty("kullaniciPassword"));
-        testCenterTechproPage.logout.click();
+        TestCenterTechproPage testCenterTechproPage = new TestCenterTechproPage();
+        testCenterTechproPage.userName.sendKeys(ConfigReader.getProperty("kullaniciAdi"), Keys.TAB,
+                ConfigReader.getProperty("kullaniciPassword"),Keys.ENTER);
         Assert.assertTrue(testCenterTechproPage.text.isDisplayed());
-
-        //        //Sayfadan cikis yap ve cikis yapildigini test et
+        //Sayfadan cikis yap ve cikis yapildigini test et
         testCenterTechproPage.logout.click();
         Assert.assertTrue(testCenterTechproPage.giris.isDisplayed());
-
-Driver.closeDriver();
     }
-}
+    }
+
