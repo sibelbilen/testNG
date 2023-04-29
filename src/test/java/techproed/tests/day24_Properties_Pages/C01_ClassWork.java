7378ab06e7a3f481979c57c186c355d4bbb1ee0e
sibelbilen <sibelinko33@gmail.com>
2023-04-29 17:48:46 +0300

    .

---
+++ src/test/java/techproed/tests/day24_Properties_Pages/C01_ClassWork.java
@@ -0,0 +1,17 @@
+package techproed.tests.day24_Properties_Pages;
+
+import org.testng.annotations.Test;
+import techproed.utilies.ConfigReader;
+import techproed.utilies.Driver;
+
+public class C01_ClassWork {
+    @Test
+    public void testName() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazon_url"));
+
+
+
+
+
+    }
+}
