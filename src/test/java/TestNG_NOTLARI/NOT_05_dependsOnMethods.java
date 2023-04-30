fec13c3cd03fc6e66d78089111006f92ea0dffed
sibelbilen <sibelinko33@gmail.com>
2023-04-30 11:39:33 +0300

    .

--- src/test/java/TestNG_NOTLARI/NOT_05_dependsOnMethods.java
+++ src/test/java/TestNG_NOTLARI/NOT_05_dependsOnMethods.java
@@ -1,4 +1,9 @@
 package TestNG_NOTLARI;
 
 public class NOT_05_dependsOnMethods {
+
+    /*
+    --dependsOnMethods BU YONTEM,bir methodun diger bir methoda bagli olmasini saglamak icin kullanilir.
+    --
+     */
 }
