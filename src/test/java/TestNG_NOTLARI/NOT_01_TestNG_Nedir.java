cc458882e3a6710319511ae742d05a3450bea41b
sibelbilen <sibelinko33@gmail.com>
2023-04-30 11:16:22 +0300

    .

---
+++ src/test/java/TestNG_NOTLARI/NOT_01_TestNG_Nedir.java
@@ -0,0 +1,18 @@
+package TestNG_NOTLARI;
+
+public class NOT_01_TestNG_Nedir {
+    /*
+
+    1-Junit'in gelismis versiyonudur.
+    2-ismi next generation test kelimelerinden turetilmis ve cendric beust tarafindan olusturulmustur.
+    3-acik kaynak kodludur.
+    4-testNG bir test kutuphanesidir.
+    5-testNG sadece java ile calisir ve JDK 7 VE DAHA UST versiyonlari gereklidir.
+    6-testNG testerlara daha fazla imkan verir ve testleri daha etkili yapmamizi saglar.
+    7-testerlar testNG'yi etkili bir framework tasarlamak ve test caseleri testNg annotationlari ile organize temek icin kullanilir.
+    8-test caseleri siralama ozelligi(priority)ve test caselerin birbirine bagimliligi(dependsonmethod)bize testleri organize etmekte yardim eder.
+    9-paralel ve cross-Browser test yapmamiza imkan saglar.
+    10-kullanisli HTML veya xml raporlari olusturmamiza yarar.
+    11-Data Provider ile DDT(data driven testing)imkani saglar.
+     */
+}
