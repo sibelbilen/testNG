734fba9314ad037ae4575d6d59176499d48c8ea8
sibelbilen <sibelinko33@gmail.com>
2023-04-30 11:24:01 +0300

    .

---
+++ src/test/java/TestNG_NOTLARI/NOT_02_nasil_YUKLENIR.java
@@ -0,0 +1,19 @@
+package TestNG_NOTLARI;
+
+public class NOT_02_nasil_YUKLENIR {
+    /*
+    1-Yeni bir maven projesi olustur : B103TestNGProject
+    2-Java nin altında bir dosya olustur : techproed
+tests -> techproed’e saga tikla : techproed.tests
+utilities -> techproed’e saga tikla : techproed.utilities
+pages-> techproed’e saga tikla : techproed.pages
+
+3-Dependency’leri pom.xml e ekle
+Get the dependencies mvnrepository.com
+selenium java-JUNITDE DE KULLANDIK
+webdrivermanager-JUNITDE DE KULLANDIK
+testNG-TESTNG FRAMEWORKU IIN KULLANACAZ
+
+     */
+
+}
---
+++ src/test/java/TestNG_NOTLARI/NOT_03_Test.java
@@ -0,0 +1,33 @@
+package TestNG_NOTLARI;
+
+public class NOT_03_Test {
+    /*
+    @Test annotation
+
+    --en cok kullanilan testNG notasyonudur.
+    --@Test: Test case oluşturmak için kullanılır.
+    --methodu test case olarak isaretler ve calistirir.
+    --ayrica main methoda ihtiyac duymaz.
+     */
+
+
+
+
+    /*
+
+    @Before ve @After: 5 Before ve 5 After annotation bulunur.
+    suite > tests > group > class > method
+    @BeforeSuite: Herbir test suitinden önce bir kez çalışır.
+    @AfterSuite: Herbir test suitinden sonra bir kez çalışır.
+    @BeforeTest: Herbir test öncesi bir kez çalışır.(Herbir test case'i öncesi değil!!!)
+    @AfterTest: Herbir test sonrası bir kez çalışır.(Herbir test case'i sonrası değil!!!)
+    @BeforeGroups: Belirtilmiş herhangi bir grup öncesi çalışır
+    @AfterGroups: Belirtilmiş herhangi bir grup sonrası çalışır
+    @BeforeClass: Herbir class'tan önce bir kez çalışır
+    @AfterClass: Herbir class'tan sonra bir kez çalışır
+    @BeforeMethod: Herbir @Test  annotation'ı öncesi bir kez çalışır.(JUnit @Before annotation'ı gibi)
+    @AfterMethod: Herbir @Test  annotation'ı sonrası bir kez çalışır.(JUnit @After annotation'ı gibi)
+
+     */
+
+}
