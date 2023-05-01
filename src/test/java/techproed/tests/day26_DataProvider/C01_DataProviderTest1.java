596a5460cb27a005fd8d3371e1a4c45176291956
sibelbilen <sibelinko33@gmail.com>
2023-05-01 11:43:44 +0300

    .

--- src/test/java/techproed/tests/day26_DataProvider/C01_DataProviderTest1.java
+++ src/test/java/techproed/tests/day26_DataProvider/C01_DataProviderTest1.java
@@ -1,6 +1,15 @@
 package techproed.tests.day26_DataProvider;
 
+import org.testng.annotations.DataProvider;
+import techproed.utilies.ExcelUtils;
+
 public class C01_DataProviderTest1 {
 
+@DataProvider()
+    public Object[][]customer(){
 
+    //excelUtilis objesi olusturarak getDataArray()methodu ile iki boyutlu string array'i DataProvider olarak return yapariz.
+    ExcelUtils excelUtils=new ExcelUtils("src/test/java/resources/mysmoketestdata.xlsx","customert_info");
+  return   excelUtils.getDataArray();
+}
 }
