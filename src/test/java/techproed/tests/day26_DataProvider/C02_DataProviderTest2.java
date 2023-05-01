468b8acded091a453c1097dc9907903521aaf0e1
sibelbilen <sibelinko33@gmail.com>
2023-05-01 12:41:46 +0300

    .

--- src/test/java/techproed/tests/day26_DataProvider/C02_DataProviderTest2.java
+++ src/test/java/techproed/tests/day26_DataProvider/C02_DataProviderTest2.java
@@ -1,8 +1,21 @@
 package techproed.tests.day26_DataProvider;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import techproed.utilies.DataProviderUtils;
+
 public class C02_DataProviderTest2 {
 
+    @Test(dataProvider = "sehirVerileri", dataProviderClass = DataProviderUtils.class)
+    public void test01(String ad,String bolge,String plaka) {
+        System.out.println("-Sehir ad->: "+ad+"-Bolge->"+bolge+"-plaka->"+plaka);
 
 
+    }
 
+    @Test(dataProvider = "kullaniciBilgileri",dataProviderClass = DataProviderUtils.class)
+    public void test02(String username,String password) {
+
+        System.out.println("kullanici ad : "+ " Sifre :  "+password);
+    }
 }
--- src/test/java/techproed/utilies/DataProviderUtils.java
+++ src/test/java/techproed/utilies/DataProviderUtils.java
@@ -1,5 +1,19 @@
 package techproed.utilies;
 
+import org.testng.annotations.DataProvider;
+
 public class DataProviderUtils {
+@DataProvider
+    public Object[][]sehirVerileri(){
+
+    return new Object[][]{{"Istanbul","Maramara","34"},
+            {"Diyarbakir","Guneydogu","21"},{"Ankara","IcAnadolu","06"}};
+}
+    @DataProvider
+
+    public  Object[][]kullaniciBilgileri(){
+    return new  Object[][]{{"Ali","Ali.123"},{"Ayse","Ayse.123"},{"Fatma","Ftm_987"}};
+    }
 
 }
+
