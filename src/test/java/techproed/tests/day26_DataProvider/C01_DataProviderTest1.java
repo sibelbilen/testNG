ebdf76491e383865201196ee759b8114785175a8
sibelbilen <sibelinko33@gmail.com>
2023-05-01 11:46:06 +0300

    .

--- src/test/java/techproed/tests/day26_DataProvider/C01_DataProviderTest1.java
+++ src/test/java/techproed/tests/day26_DataProvider/C01_DataProviderTest1.java
@@ -9,7 +9,24 @@ public class C01_DataProviderTest1 {
     public Object[][]customer(){
 
     //excelUtilis objesi olusturarak getDataArray()methodu ile iki boyutlu string array'i DataProvider olarak return yapariz.
-    ExcelUtils excelUtils=new ExcelUtils("src/test/java/resources/mysmoketestdata.xlsx","customert_info");
+
+
+    ExcelUtils excelUtils=
+            new ExcelUtils("src/test/java/resources/mysmoketestdata.xlsx","customert_info");
   return   excelUtils.getDataArray();
 }
+
+//ikinci yol:
+@DataProvider()
+public Object[][]customer1(){
+
+    //excelUtilis objesi olusturarak getDataArray()methodu ile iki boyutlu string array'i DataProvider olarak return yapariz.
+String path="src/test/java/resources/mysmoketestdata.xlsx";
+String sheetName="customert_info";
+
+    ExcelUtils excelUtils=
+            new ExcelUtils(path,sheetName);
+    return   excelUtils.getDataArray();
+}
+
 }
