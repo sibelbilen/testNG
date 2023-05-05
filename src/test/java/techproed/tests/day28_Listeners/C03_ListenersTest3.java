1a10af21c43636bd6963f6f15f9b0057f8241640
sibelbilen <sibelinko33@gmail.com>
2023-05-05 12:13:26 +0300

    .

--- src/test/java/techproed/tests/day28_Listeners/C03_ListenersTest3.java
+++ src/test/java/techproed/tests/day28_Listeners/C03_ListenersTest3.java
@@ -8,33 +8,34 @@ import techproed.utilies.ConfigReader;
 import techproed.utilies.Driver;
 
 public class C03_ListenersTest3 {
-    @Test
+
+    /*
+      Test class'ımızda fail olma durumu olan bir testin listeners ile tekrar çalışmasını istersek
+  @Test notasyanundan sonra parametre olarak retryAnalyzer yazar ve oluşturmuş olduğumuz ListenersRetry class'ının
+  yolunu belirtiriz.
+   */
+    @Test(retryAnalyzer = techproed.utilies.ListenersRetry.class)
     public void test1() {
         System.out.println("PASSED");
         Assert.assertTrue(true);
         Assert.assertEquals(2,2);
     }
-
-    @Test
+    @Test(retryAnalyzer = techproed.utilies.ListenersRetry.class)
     public void test2() {
         System.out.println("FAILED");
         Assert.assertTrue(false);
     }
-
-    @Test
+    @Test(retryAnalyzer = techproed.utilies.ListenersRetry.class)
     public void test3() {
         System.out.println("SKIP");
         throw new SkipException("Method atlandı");//Testi atlar yani ignore eder
     }
-
-    @Test
+    @Test(retryAnalyzer = techproed.utilies.ListenersRetry.class)
     public void test4() {
         System.out.println("NO SUCH ELEMENT EXCEPTION");
         //throw new NoSuchElementException("NOSUCHELEMENTEXCEPTION");
         Driver.getDriver().get(ConfigReader.getProperty("amazon_Url"));
         Driver.getDriver().findElement(By.xpath("lksdlfkjsldkjfs"));
-
     }
-
 
 }
