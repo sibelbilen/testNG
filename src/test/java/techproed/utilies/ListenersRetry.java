425d40dc06dbe98e24593b4b6a5f1a2a8aa1948b
sibelbilen <sibelinko33@gmail.com>
2023-05-05 12:04:20 +0300

    .

---
+++ src/test/java/techproed/utilies/ListenersRetry.java
@@ -0,0 +1,24 @@
+package techproed.utilies;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class ListenersRetry implements IRetryAnalyzer {
+
+
+    /*
+    Bu sınıf sadece FAIL olan test case'leri tekrar çalıştırır
+    maxRetryCount ek olarak çalisma sayısıdır. Bu örnekte Fail olan test (maxRetryCount = 1) normal bir kere
+    çalıştıktan sonra fail olursa 1 kez daha çalışacaktır.
+     */
+        private int retryCount = 0;
+        private static final int maxRetryCount = 2;
+        @Override
+        public boolean retry(ITestResult result) {
+            if (retryCount < maxRetryCount) {
+                retryCount++;
+                return true;
+            }
+            return false;
+        }
+}
