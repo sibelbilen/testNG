d23195fd865542d9e43ec3e363f64e9c23495d56
sibelbilen <sibelinko33@gmail.com>
2023-05-06 18:04:56 +0300

    .

--- src/test/java/techproed/tests/day29_Listeners/C02_ListenersTest2.java
+++ src/test/java/techproed/tests/day29_Listeners/C02_ListenersTest2.java
@@ -2,13 +2,16 @@ package techproed.tests.day29_Listeners;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import techproed.pages.BlueRentalPage;
 import techproed.utilies.ConfigReader;
 import techproed.utilies.Driver;
 
 //Listeners eklemeden çalıştırdık, sadece fail durumunda tekrar çalıştırdı..
-// Ama Listeners ekleyince class üzerine screenshot da yaptı ve resimleri çekerek klasör içerisinde verdi
+// Ama Listeners ekleyince class üzerine screenshot da yaptı ve resimleri çekerek klasör
+// içerisinde verdi
+@Listeners(techproed.utilies.Listeners.class)
 public class C02_ListenersTest2 {
     @Test(retryAnalyzer = techproed.utilies.Listeners.class)//Eğer test fail olursa Listeners class'ındaki
     // retry methodu sayesinde 1 kez daha çalışır
