220332c609fa759882693498157fbd76d942c79d
sibelbilen <sibelinko33@gmail.com>
2023-05-06 17:49:26 +0300

    .

--- src/test/java/techproed/tests/day29_Listeners/C01_ListenersTest1.java
+++ src/test/java/techproed/tests/day29_Listeners/C01_ListenersTest1.java
@@ -1,4 +1,37 @@
 package techproed.tests.day29_Listeners;
 
-public class C01_ListenersTest1 {
+import org.openqa.selenium.By;
+import org.testng.*;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import techproed.utilies.ConfigReader;
+import techproed.utilies.Driver;
+
+@Listeners(techproed.utilies.Listeners.class)
+public class C01_ListenersTest1   {
+
+
+        @Test(retryAnalyzer = techproed.utilies.Listeners.class)
+        public void test1() {
+            System.out.println("PASSED");
+            Assert.assertTrue(true);
+            Assert.assertEquals(2,2);
+        }
+        @Test(retryAnalyzer = techproed.utilies.Listeners.class)
+        public void test2() {
+            System.out.println("FAILED");
+            Assert.assertTrue(false);
+        }
+        @Test(retryAnalyzer = techproed.utilies.Listeners.class)
+        public void test3() {
+            System.out.println("SKIP");
+            throw new SkipException("Method atlandı");//Testi atlar yani ignore eder
+        }
+        @Test(retryAnalyzer = techproed.utilies.Listeners.class)
+        public void test4() {
+            System.out.println("NO SUCH ELEMENT EXCEPTION");
+            //throw new NoSuchElementException("NOSUCHELEMENTEXCEPTION");
+            Driver.getDriver().get(ConfigReader.getProperty("amazon_Url"));
+            Driver.getDriver().findElement(By.xpath("lksdlfkjsldkjfs"));
+        }
 }
---
+++ src/test/java/techproed/tests/day29_Listeners/C02_ListenersTest2.java
@@ -0,0 +1,31 @@
+package techproed.tests.day29_Listeners;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import techproed.pages.BlueRentalPage;
+import techproed.utilies.ConfigReader;
+import techproed.utilies.Driver;
+
+//Listeners eklemeden çalıştırdık, sadece fail durumunda tekrar çalıştırdı..
+// Ama Listeners ekleyince class üzerine screenshot da yaptı ve resimleri çekerek klasör içerisinde verdi
+public class C02_ListenersTest2 {
+    @Test(retryAnalyzer = techproed.utilies.Listeners.class)//Eğer test fail olursa Listeners class'ındaki
+    // retry methodu sayesinde 1 kez daha çalışır
+    public void test1() {
+        Driver.getDriver().get(ConfigReader.getProperty("techproed_Url"));
+        Driver.getDriver().findElement(By.xpath("//*[class='sdlkfsd']"));//yanlış locate aldık
+    }
+    @Test(retryAnalyzer = techproed.utilies.Listeners.class)
+    public void test2() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazon_Url"));
+        Driver.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
+    }
+    @Test(retryAnalyzer = techproed.utilies.Listeners.class)
+    public void test3() {
+        Driver.getDriver().get(ConfigReader.getProperty("blueRentACarUrl"));
+        BlueRentalPage blueRentalPage = new BlueRentalPage();
+        blueRentalPage.login.click();
+        blueRentalPage.email.sendKeys("erol@gmail.com", Keys.TAB,"12345",Keys.ENTER);
+    }
+}
